73750a35d272ac41b209c54eb4289876f301febc
YounHyeJu <108314244+YounHyeJu@users.noreply.github.com>
2022-11-08 11:21:54 +0900

    Initial commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,36 @@
+import {
+  ThemeProvider,
+  useBreakpoint,
+  useColorMode,
+  useTheme,
+} from '@chakra-ui/react';
+
+import ToggleColorModeButton from '@components/common/ToggleColorModeButton';
+import TokDocsDevTools from '@components/common/TokDocsDevTool';
+
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { mode } from '@theme/foundations/colors';
+
+import withAppProvider from 'contexts/app/app.provider';
+import { withGlobalModalHandlerContext } from 'contexts/modal/useGlobalModalHandler.context';
+
+function MyApp({ Component, pageProps }: any) {
+  const theme = useTheme();
+  const { colorMode } = useColorMode();
+  const br = useBreakpoint();
+  console.log({ br });
+
+  return (
+    // Provide the client to your App
+    <ThemeProvider
+      theme={{ ...theme, colors: { ...theme.colors, ...mode[colorMode] } }}
+    >
+      <ToggleColorModeButton />
+      <Component {...pageProps} />
+      <ReactQueryDevtools initialIsOpen={false} />
+      <TokDocsDevTools />
+    </ThemeProvider>
+  );
+}
+
+export default withAppProvider(withGlobalModalHandlerContext(MyApp));
