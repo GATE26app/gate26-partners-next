2c581b4eb7a75d151174d2dba36825a056889e2c
Lee Hyeon gyu <gusrb0405@naver.com>
2022-11-16 19:38:31 +0900

    eslint오타 수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -33,7 +33,7 @@ module.exports = {
     '@typescript-eslint/no-empty-interface': 'off',
     '@typescript-eslint/no-namespace': 'off',
     'prettier/prettier': [
-      'errror',
+      'error',
       {
         endOfLine: 'auto',
       },
