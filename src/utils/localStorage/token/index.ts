19e78d62855a385430e8016eb096eaaab9756d8e
양혜지 <haejidev@gmail.com>
2022-12-23 09:41:48 +0900

    feat: 🎸프론트 기본 세팅

--- src/utils/localStorage/token/index.ts
+++ src/utils/localStorage/token/index.ts
@@ -14,15 +14,33 @@ export type TokenType = {
   isRegister: boolean;
 };
 
+const GateUserDefaultKey = {
+  _JWT_TOKEN: 'jwtToken',
+  _USER_ID: 'userId',
+};
+
 export const getToken = () => {
-  const token = getLocalStorage<TokenType>(TOKEN_KEY);
+  const token = getLocalStorage<string>(GateUserDefaultKey._JWT_TOKEN);
   return token;
 };
 
-export const setToken = (token: TokenType) => {
-  setLocalStorage(TOKEN_KEY, token);
+export const setToken = (token: string) => {
+  setLocalStorage(GateUserDefaultKey._JWT_TOKEN, token);
 };
 
 export const deleteToken = () => {
-  removeLocalStorage(TOKEN_KEY);
+  removeLocalStorage(GateUserDefaultKey._JWT_TOKEN);
+};
+
+export const getUserId = () => {
+  const token = getLocalStorage<string>(GateUserDefaultKey._USER_ID);
+  return token;
+};
+
+export const setUserId = (userId: string) => {
+  setLocalStorage(GateUserDefaultKey._USER_ID, userId);
+};
+
+export const deleteUserId = () => {
+  removeLocalStorage(GateUserDefaultKey._USER_ID);
 };
