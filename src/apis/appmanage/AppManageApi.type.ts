898c795368a55947a4b05a73ea9006007ef3722e
양혜지 <haejidev@gmail.com>
2022-12-23 09:43:16 +0900

    feat: 🎸앱 버전 추가하기 기능 개발

---
+++ src/apis/appmanage/AppManageApi.type.ts
@@ -0,0 +1,37 @@
+import { StringLocale } from 'yup/lib/locale';
+
+export type AppVersionInfoDTOType = {
+  deviceType: string;
+  majorVersion: string;
+  minorVersion: string;
+  patchVersion: string;
+  descText: string;
+  versionStatus: string;
+};
+
+export type DefaultDTOType = {
+  code?: string;
+  data?: string;
+  message?: StringLocale;
+  count?: number;
+  success: boolean;
+};
+
+export type AppVersionInfoDTO = {
+  code?: string;
+  data?: AppVesionInfo;
+  message?: StringLocale;
+  count?: number;
+  success: boolean;
+};
+
+export type AppVesionInfo = {
+  id: number;
+  deviceType: string;
+  majorVersion: number;
+  minorVersion: number;
+  patchVersion: number;
+  descText: string;
+  versionStatus: string;
+  modifiedDate: string;
+};
