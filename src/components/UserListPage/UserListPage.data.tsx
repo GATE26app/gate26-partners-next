fe77424cf89e2713587ba607e65818a5c4b9f5aa
OhHanui <ohhn@asianaidt.com>
2022-12-23 17:29:20 +0900

    feat: 🎸회원목록 조회 개발

--- src/components/UserListPage/UserListPage.data.tsx
+++ src/components/UserListPage/UserListPage.data.tsx
@@ -5,19 +5,21 @@ import {
   DataTableRowType,
 } from '@components/common/DataTable';
 
+import { crypto } from '@utils/crypto';
+
 export type UserColumnType =
-  | 'id'
-  | 'profile'
+  | 'userId'
+  | 'profileImagePath'
+  | 'nickName'
   | 'name'
-  | 'nickname'
-  | 'enName'
+  | 'enLastName'
+  | 'enFirstName'
   | 'gender'
-  | 'email'
-  | 'phoneNumber'
-  | 'birthday'
-  | 'signupPath'
-  | 'createdAt'
-  | 'lastedAt';
+  | 'birthDate'
+  | 'phone'
+  | 'emailAddress'
+  | 'createdDate'
+  | 'lastAccessDate';
 
 class UserColumns {
   onChange?: (key: string, value: string | number) => void;
@@ -30,12 +32,13 @@ class UserColumns {
 
   readonly LIST_COLUMNS: DataTableColumnType<UserColumnType>[] = [
     {
-      key: 'profile',
+      key: 'profileImagePath',
       name: '프로필 이미지',
       width: '7.08%',
       render: (value: DataTableRowType<UserColumnType>) => (
         <Image
-          src={value.profile as string}
+          // src={value.profileImagePath as string}
+          src={`http://dresource.gate26.co.kr/img/downloadFile?filePath=${value.profileImagePath}`}
           w="40px"
           h="40px"
           borderRadius="50%"
@@ -47,37 +50,55 @@ class UserColumns {
       key: 'name',
       name: '이름',
       width: '6.67%',
+      render: (value: DataTableRowType<UserColumnType>) => {
+        return crypto.decrypt(value.name as string)
+      }
     },
     {
-      key: 'nickname',
+      key: 'nickName',
       name: '닉네임',
       width: '10%',
     },
-
     {
-      key: 'enName',
+      key: 'enLastName',
       name: '영문명',
       width: '10%',
+      render: (value: DataTableRowType<UserColumnType>) => {
+        let lastName = "";
+        let firstName = "";
+        if(value.enLastName != undefined) lastName = value.enLastName as string;
+        if(value.enFirstName != undefined) firstName = value.enFirstName as string;
+        return lastName + " " + firstName;
+      }
     },
     {
       key: 'gender',
       name: '성별',
       width: '4.17%',
+      render: (value: DataTableRowType<UserColumnType>) => {
+        return value.gender === 'F'? '여' : '남';
+      }
     },
     {
-      key: 'email',
+      key: 'emailAddress',
       name: '이메일',
       width: '17.08%',
     },
     {
-      key: 'phoneNumber',
+      key: 'phone',
       name: '핸드폰 번호',
       width: '10%',
+      render: (value: DataTableRowType<UserColumnType>) => {
+        return crypto.decrypt(value.phone as string)
+      }
     },
     {
-      key: 'birthday',
+      key: 'birthDate',
       name: '생년월일',
       width: '8.33%',
+      render: (value: DataTableRowType<UserColumnType>) => {
+        return crypto.decrypt(value.birthDate as string)
+      }
     },
     {
       key: 'signupPath',
@@ -85,12 +106,12 @@ class UserColumns {
       width: '8.33%',
     },
     {
-      key: 'createdAt',
+      key: 'createdDate',
       name: '생성일자',
       width: '8.33%',
     },
     {
-      key: 'lastedAt',
+      key: 'lastAccessDate',
       name: '최종접속일자',
       width: '8.33%',
     },
---
+++ src/utils/crypto.ts
@@ -0,0 +1,21 @@
+
+import cryptoJs from 'crypto-js'
+
+const key = btoa('IDT-ReGate26');
+
+export const crypto: any = {
+  encrypt: (string: any | undefined) => {
+    var aes128Iv = '0123456789abcdef';
+
+    return cryptoJs.AES?.encrypt(string, cryptoJs.enc.Utf8.parse(key), {
+      iv: cryptoJs.enc.Utf8.parse(aes128Iv),
+    }).toString();
+  },
+
+  decrypt: (encStr: any | undefined) => {
+    var aes128Iv = '0123456789abcdef';
+    return cryptoJs.AES?.decrypt(encStr, cryptoJs.enc.Utf8.parse(key), {
+      iv: cryptoJs.enc.Utf8.parse(aes128Iv),
+    }).toString(cryptoJs.enc.Utf8);
+  },
+};
